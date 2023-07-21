779261a4d9adbc42302538eafaf868996f96fbf8
korundum1001 <madanebook@gmail.com>
2023-07-21 09:38:38 +0800

    set route path for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import React from "react"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useRoutes
+} from "react-router-dom";
 import './App.css'
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminAddCars from "./pages/AdminAddCars";
+import AdminEditCars from "./pages/AdminEditCars";
 
 function App() {
+  let element = useRoutes ([
+    {path: "/", element: <AdminLogin/>},
+    {path: "/admin-dashboard", element: <AdminDashboard/>},
+    {path: "/admin-addcars", element: <AdminAddCars/>},
+    {path: "/admin-editcars", element: <AdminEditCars/>},
+  ])
 
-  return (
-    <>
-      
-    </>
-  )
+  return element
 }
 
 export default App
